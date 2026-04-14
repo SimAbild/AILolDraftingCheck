f47a95e4df205c8ff8c29f684f564c6a9f16558b
SimAbild <abildtrup123@gmail.com>
2026-04-14 14:50:50 +0200

    Initial commit

---
+++ src/main/java/org/example/ailoldraftingcheck/AiLolDraftingCheckApplication.java
@@ -0,0 +1,13 @@
+package org.example.ailoldraftingcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLolDraftingCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLolDraftingCheckApplication.class, args);
+	}
+
+}
