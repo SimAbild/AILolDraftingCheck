e12591b0a73a30cb974e8ba1c3d435b391878c0f
MU-1995 <jynke58@hotmail.com>
2026-04-21 12:35:07 +0200

    CoachResponse klasse

--- src/main/java/org/example/ailoldraftingcheck/dtos/CoachResponse.java
+++ src/main/java/org/example/ailoldraftingcheck/dtos/CoachResponse.java
@@ -1,4 +1,37 @@
 package org.example.ailoldraftingcheck.dtos;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class CoachResponse {
+    private List<String> positives;
+    private List<String> negatives;
+    private List<Alternative> alternatives;
+
+    public CoachResponse(List<String> positives, List<String> negatives, List<Alternative> alternatives) {
+        this.positives = positives;
+        this.negatives = negatives;
+        this.alternatives = alternatives;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Alternative {
+        private String championName;
+        private String iconUrl;
+        private String reason;
+
+        public Alternative(String championName, String iconUrl, String reason) {
+            this.championName = championName;
+            this.iconUrl = iconUrl;
+            this.reason = reason;
+        }
+    }
 }
