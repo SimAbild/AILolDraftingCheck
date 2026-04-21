a784cc4b1e3a0caa383cbebe88b32eef6af73e48
MU-1995 <jynke58@hotmail.com>
2026-04-21 12:33:55 +0200

    CoachRequest klasse

--- src/main/java/org/example/ailoldraftingcheck/dtos/CoachRequest.java
+++ src/main/java/org/example/ailoldraftingcheck/dtos/CoachRequest.java
@@ -1,5 +1,17 @@
 package org.example.ailoldraftingcheck.dtos;
 
-public class CoachRequest {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CoachRequest {
+    private String userRole;
+    private String userChampion;
+    private List<DraftPickRequest> allyTeam;
+    private List<DraftPickRequest> enemyTeam;
 }
