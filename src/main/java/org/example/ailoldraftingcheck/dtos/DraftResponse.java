1b326d40d9c582598d34dc4d54bc3bdc6d2d86bd
MU-1995 <jynke58@hotmail.com>
2026-04-21 12:42:44 +0200

    DraftPickResponse klasse

--- src/main/java/org/example/ailoldraftingcheck/dtos/DraftResponse.java
+++ src/main/java/org/example/ailoldraftingcheck/dtos/DraftResponse.java
@@ -1,4 +1,22 @@
 package org.example.ailoldraftingcheck.dtos;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class DraftResponse {
+    private String userRole;
+    private List<DraftPickRequest> enemyTeam;
+    private List<DraftPickRequest> allyTeam;
+
+    public DraftResponse(String userRole, List<DraftPickRequest> enemyTeam, List<DraftPickRequest> allyTeam) {
+        this.userRole = userRole;
+        this.enemyTeam = enemyTeam;
+        this.allyTeam = allyTeam;
+    }
 }
