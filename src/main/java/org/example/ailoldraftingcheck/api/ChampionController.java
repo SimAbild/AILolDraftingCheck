1d095df95f2a9ba68b6825b024a2a9fa72da7e80
MU-1995 <jynke58@hotmail.com>
2026-04-21 12:43:46 +0200

    ChampionController klasse

--- src/main/java/org/example/ailoldraftingcheck/api/ChampionController.java
+++ src/main/java/org/example/ailoldraftingcheck/api/ChampionController.java
@@ -1,4 +1,24 @@
 package org.example.ailoldraftingcheck.api;
 
+import org.example.ailoldraftingcheck.dtos.Champion;
+import org.example.ailoldraftingcheck.service.DataDragonService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/champions")
+@CrossOrigin(origins = "*")
 public class ChampionController {
+
+    private final DataDragonService dataDragonService;
+
+    public ChampionController(DataDragonService dataDragonService) {
+        this.dataDragonService = dataDragonService;
+    }
+
+    @GetMapping
+    public List<Champion> getChampions() {
+        return dataDragonService.getAllChampions();
+    }
 }
